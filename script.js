81be923753fb2d58be7ec46f4a9e9b3827fd7f81
Maggie1111111 <margrethe.koller@outlook.com>
2025-02-04 21:05:52 +0100

    score og javascript på slange og poeng

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
+const container = document.querySelector(".spillbrett")
+const startknapp = document.querySelector(".container button")
+const containerspillbrett = document.querySelector(".containerspillbrett")
+
 for (x=0; x<225; x++){
-    const container = document.querySelector(".spillbrett")
     const div = document.createElement("div")
     container.appendChild(div)
 }
+
+function slange(){
+    const slange = document.createElement("div")
+    slange.style.backgroundColor = "pink"
+    
+    slange.style.position = "absolute"
+    slange.style.width = "99.9px"
+    slange.style.height = "33.3px"
+    slange.style.left = "33.3px"
+    containerspillbrett.appendChild(slange)
+}
+function poeng(){
+    const poeng = document.createElement("div")
+    poeng.style.backgroundColor = "orange"
+    poeng.style.position = "absolute"
+    poeng.style.width = "33.3px"
+    poeng.style.height = "33.3px"
+    poeng.style.right = "33.3px"
+    containerspillbrett.appendChild(poeng)
+}
+
+function start(){
+    slange()
+    poeng()
+}
+startknapp.addEventListener("click", start)
