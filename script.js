b56ebe6280836bc551bdf97324656940622f74fe
Maggie1111111 <margrethe.koller@outlook.com>
2025-02-05 12:26:07 +0100

    fiksa noen bugs

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-const container = document.querySelector(".spillbrett")
+const brett = document.querySelector(".spillbrett")
 const startknapp = document.querySelector(".container button")
 const containerspillbrett = document.querySelector(".containerspillbrett")
 const logo = document.getElementById("logo")
 
+let slangeArray = [{x : 10, y : 10}]
 
-
-function slange(){
-    const slange = document.createElement("div")
-    slange.style.backgroundColor = "pink"
+function tegnSlange(){
+    slangeArray.forEach((segment) => {
+        const slangeElement = document.createElement("div")
+        slangeElement.style.backgroundColor = "pink"
     
-    slange.style.position = "absolute"
-    slange.style.width = "33.3px"
-    slange.style.height = "33.3px"
-    slange.style.left = "33.3px"
-    slange.style.border = "dotted 1px #2f503a"
-    containerspillbrett.appendChild(slange)
+        slangeElement.style.position = "absolute"
+        slangeElement.style.width = "33.3px"
+        slangeElement.style.height = "33.3px"
+        slangeElement.style.left = "33.3px"
+        slangeElement.style.border = "dotted 1px #58916a"
+        containerspillbrett.appendChild(slangeElement)
+    });
 }
+
 function poeng(){
     const poeng = document.createElement("div")
     poeng.style.backgroundColor = "orange"
@@ -28,9 +31,14 @@ function poeng(){
 
 function start(){
     logo.style.display = "none"
+
+    if (brett.children.length === 225) {
+        return
+    }
+
     for (x=0; x<225; x++){
         const div = document.createElement("div")
-        container.appendChild(div)
+        brett.appendChild(div)
     }
     slange()
     poeng()
