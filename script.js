4453205b4c7402abd7c5f884a465d5260c6bb9c8
Maggie1111111 <margrethe.koller@outlook.com>
2025-02-10 09:50:19 +0100

    slange bevege seg, noe debugging,

--- script.js
+++ script.js
@@ -2,21 +2,27 @@ const brett = document.querySelector(".spillbrett")
 const startknapp = document.querySelector(".container button")
 const containerspillbrett = document.querySelector(".containerspillbrett")
 const logo = document.getElementById("logo")
-let retning = ""
+let retning = "right"
+let spillInterval
 
 
 
-let slangeArray = [{x : 7, y : 2}]
+let slangeArray = [{x : 7, y : 7}]
 
 function tegnSlange(){
+    document.querySelectorAll(".slange").forEach(e => e.remove())
+
     slangeArray.forEach((segment) => {
         const slangeHode = document.createElement("div")
         slangeHode.style.backgroundColor = "pink"
+        slangeHode.classList.add("slange")
     
         slangeHode.style.position = "absolute"
         slangeHode.style.width = "33.3px"
-        slangeHode.style.height = "33.3px"
-        slangeHode.style.left = "33.3px"
+        slangeHode.style.height = "33.3px";
+
+        slangeHode.style.left = `${segment.x * 33.3}px`;
+        slangeHode.style.top = `${segment.y * 33.3}px`
         containerspillbrett.appendChild(slangeHode)
     })
 }
@@ -32,12 +38,25 @@ function poeng(){
     containerspillbrett.appendChild(poeng)
 }
 
+function flyttSlange() {
+    let hode = { ...slangeArray[0] }
+    
+    if (retning === "up") hode.y -= 1
+    if (retning === "down") hode.y += 1
+    if (retning === "left") hode.x -= 1
+    if (retning === "right") hode.x += 1
 
+    slangeArray.unshift(hode)
+    slangeArray.pop()
+
+    tegnSlange()
+}
 document.addEventListener("keydown", function(event){
-    const taster = {PilOpp:"up", PilNed:"down", PilVenstre:"left", PilHøyre:"right"}
+    const taster = {ArrowUp:"up", ArrowDown:"down", ArrowLeft:"left", ArrowRight:"right"}
     if (taster[event.key]){
         retning = taster[event.key]
     }
+    console.log(retning)
     
 })
 
@@ -54,6 +73,12 @@ function start(){
     }
     tegnSlange()
     poeng()
+    
+    slangeArray = [{ x: 7, y: 7 }]
+    retning = "right"
+
+    clearInterval(spillInterval)
+    spillInterval = setInterval(flyttSlange, 200)
 
 }
 startknapp.addEventListener("click", start)
